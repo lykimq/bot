91a1c03afdea9b832e008bbf360d235340cc2a4c
Théo Zimmermann <theo.zimmermann@telecom-paris.fr>
2025-07-04 10:40:09 +0200

    Invert the logic of the auto-minimization suggestions.

--- src/actions.ml
+++ src/actions.ml
@@ -1300,10 +1300,10 @@ let suggest_ci_minimization_for_pr = function
       RunAutomatically
   | {labels} when List.exists ~f:(String.equal "kind: infrastructure") labels ->
       Silent "this PR is labeled with kind: infrastructure"
-  | {body} when String.is_substring ~substring:"offer-minimizer: off" body ->
+  | {body} when not (String.is_substring ~substring:"offer-minimizer: on" body) ->
       Silent
-        "the PR body contains an 'offer-minimizer: off' directive, which turns \
-         off minimization suggestions"
+        "the PR body does not contain an 'offer-minimizer: on' directive, which turns \
+         on minimization suggestions"
   | {draft= true} ->
       Suggest
   | _ ->
